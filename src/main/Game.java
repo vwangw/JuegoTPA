b1c4afa10d6646daa66466d72d87e8661c5d2a0b
vwangw <vwangw@alumnos.nebrija.es>
2023-11-20 17:23:39 +0100

    creacion de niveles

--- src/main/Game.java
+++ src/main/Game.java
@@ -15,7 +15,7 @@ public class Game implements Runnable{
     private LevelManager levelManager;
 
     public final static int TILES_DEFAULT_SIZE=32;
-    public final static float SCALE=1.5f;
+    public final static float SCALE=2f;
     public final static int TILES_IN_WIDTH=26;
     public final static int TILES_IN_HEIGHT=14;
     public final static int TILES_SIZE=(int)(TILES_DEFAULT_SIZE * SCALE);
@@ -52,8 +52,8 @@ public class Game implements Runnable{
 
     public void render(Graphics g){
 
-        player.render(g);
         levelManager.draw(g);
+        player.render(g);
     }
 
     @Override
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -1,6 +1,9 @@
 package utilz;
 
+import main.Game;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +12,7 @@ public class LoadSave {
 
     public static final String PLAYER_ATLAS = "player_sprites.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
+    public static final String LEVEL_ONE_DATA = "level_one_data.png";
 
     public static BufferedImage getSpriteAtlas(String fileName){
         BufferedImage img=null;
@@ -26,5 +30,22 @@ public class LoadSave {
             }
         }
         return img;
+    }
+
+    public static int [][] getLevelData(){
+        int[][] lvlData=new int [Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+        BufferedImage img=getSpriteAtlas(LEVEL_ONE_DATA);
+
+        for(int j=0;j<img.getHeight();j++){
+            for(int i=0;i< img.getWidth();i++){
+                Color color=new Color(img.getRGB(i,j));
+                int value=color.getRed();
+                if(value>=48){
+                    value=0;
+                }
+                lvlData[j][i]=value;
+            }
+        }
+        return lvlData;
     }
 }
